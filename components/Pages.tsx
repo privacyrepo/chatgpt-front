0ae41c6b648e3ec63a3a42a6ea19760f492568e1
privacyrepo <112838080+privacyrepo@users.noreply.github.com>
2023-04-08 13:06:39 +0800

    sync

--- components/Pages.tsx
+++ components/Pages.tsx
@@ -8,6 +8,8 @@ import { Link } from '@/components/util/Link';
 import { SystemPurposeId, SystemPurposes } from '@/lib/data';
 import { useChatStore, useConversationNames } from '@/lib/store-chats';
 
+import { useTranslation } from 'next-i18next';
+
 
 /**
  * FIXME - TEMPORARY - placeholder for a proper Pages Drawer
@@ -20,61 +22,62 @@ export function PagesMenu(props: { pagesMenuAnchor: HTMLElement | null, onClose:
 
   const handleConversationClicked = (conversationId: string) => setActiveConversation(conversationId);
 
+  const { t } = useTranslation("common");
   return <Menu
-    variant='plain' color='neutral' size='lg' placement='bottom-start' sx={{ minWidth: 280 }}
-    open={!!props.pagesMenuAnchor} anchorEl={props.pagesMenuAnchor} onClose={props.onClose}
-    disablePortal={false}>
+  variant='plain' color='neutral' size='lg' placement='bottom-start' sx={{ minWidth: 280 }}
+  open={!!props.pagesMenuAnchor} anchorEl={props.pagesMenuAnchor} onClose={props.onClose}
+  disablePortal={false}>
 
-    <ListItem>
-      <Typography level='body2'>
-        Active chats
+  <ListItem>
+    <Typography level='body2'>
+      {t('applicationBar.activeChats')}
+    </Typography>
+  </ListItem>
+
+  {conversationNames.map((conversation) => (
+    <MenuItem
+      key={'c-id-' + conversation.id}
+      onClick={() => handleConversationClicked(conversation.id)}
+    >
+
+      <ListItemDecorator>
+        {SystemPurposes[conversation.systemPurposeId]?.symbol || ''}
+      </ListItemDecorator>
+
+      <Typography sx={{ mr: 2 }}>
+        {conversation.name}
       </Typography>
-    </ListItem>
 
-    {conversationNames.map((conversation) => (
-      <MenuItem
-        key={'c-id-' + conversation.id}
-        onClick={() => handleConversationClicked(conversation.id)}
-      >
+      <IconButton
+        variant='soft' color='neutral' sx={{ ml: 'auto' }}
+        onClick={e => props.onClearConversation(e, conversation.id)}>
+        <DeleteOutlineIcon />
+      </IconButton>
 
-        <ListItemDecorator>
-          {SystemPurposes[conversation.systemPurposeId]?.symbol || ''}
-        </ListItemDecorator>
-
-        <Typography sx={{ mr: 2 }}>
-          {conversation.name}
-        </Typography>
-
-        <IconButton
-          variant='soft' color='neutral' sx={{ ml: 'auto' }}
-          onClick={e => props.onClearConversation(e, conversation.id)}>
-          <DeleteOutlineIcon />
-        </IconButton>
-
-      </MenuItem>
-    ))}
-
-    <MenuItem disabled={true}>
-      <ListItemDecorator><AddIcon /></ListItemDecorator>
-      <Typography sx={{ opacity: 0.5 }}>
-        New chat (soon)
-        {/* We need stable Chat and Message IDs, and one final review to the data structure of Conversation for future-proofing */}
-      </Typography>
     </MenuItem>
+  ))}
+
+  <MenuItem disabled={true}>
+    <ListItemDecorator><AddIcon /></ListItemDecorator>
+    <Typography sx={{ opacity: 0.5 }}>
+      {t('applicationBar.newChat')}
+      {/* We need stable Chat and Message IDs, and one final review to the data structure of Conversation for future-proofing */}
+    </Typography>
+  </MenuItem>
 
 
-    <ListItem>
-      <Typography level='body2'>
-        Scratchpad
-      </Typography>
-    </ListItem>
+  <ListItem>
+    <Typography level='body2'>
+      {t('applicationBar.scratchpad')}
+    </Typography>
+  </ListItem>
 
-    <MenuItem>
-      <ListItemDecorator />
-      <Typography sx={{ opacity: 0.5 }}>
-        Feature <Link href='https://github.com/enricoros/nextjs-chatgpt-app/issues/17' target='_blank'>#17</Link>
-      </Typography>
-    </MenuItem>
+  <MenuItem>
+    <ListItemDecorator />
+    <Typography sx={{ opacity: 0.5 }}>
+      {t('applicationBar.feature')} <Link href='' target='_blank'>#17</Link>
+    </Typography>
+  </MenuItem>
 
-  </Menu>;
+</Menu>;
 }
